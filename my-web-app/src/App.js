4777ac033869d62c7f71d4237157253649f21d1e
Athitaya <athitaya.bo@kkumail.com>
2025-02-18 00:29:34 +0700

    update

--- my-web-app/src/App.js
+++ my-web-app/src/App.js
@@ -1,15 +1,11 @@
+// src/App.js
 import React from 'react';
-import { Grid, Paper, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} md={4}>
-        <Paper elevation={3} style={{ padding: '20px' }}>
-          <Typography variant="h6">Responsive Box</Typography>
-        </Paper>
-      </Grid>
-    </Grid>
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
   );
 }
 
--- my-web-app/src/index.js
+++ my-web-app/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // use this for React 18 or above
 import './index.css';
-import App from './App';
+import App from './App'; // Your App component
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // for React 18 or above
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
